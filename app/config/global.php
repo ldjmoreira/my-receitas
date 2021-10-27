0f13bf6ad689b14a05a1946834300cd822a5ac4d
ldjmoreira <ldjmoreira@hotmail.com>
2021-10-27 10:18:15 -0300

    first commit

---
+++ app/config/global.php
@@ -0,0 +1,9 @@
+<?php
+
+define('BASE', '/my-receitas/');
+define('UNSET_COUNT', 1);
+
+define('DB_HOST', 'localhost:3307');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
+define('DB_NAME', 'myreceitas');
